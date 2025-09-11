d2cac14ce44ddfc5f24863dd7e16aa9f8768fda1
Möllinatorn3000 <arvid.moller@elev.ga.ntig.se>
2025-09-11 16:40:36 +0200

    jag gjorde grejer

--- app.rb
+++ app.rb
@@ -1,1 +1,17 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'slim'
+
+get("/") do
+  slim(:home)
+end
+
+get("/about") do
+  @fruits = ["Äpple", "Banan", "Päron"]
+  slim(:about)
+end
+
+get("/fruits/:id") do
+  @fruit = params[:id].to_s
+  slim(:fruits_id)
+end
