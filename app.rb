74b684761c7c3ef9e3177c52affa625b895f4e97
Möllinatorn3000 <arvid.moller@elev.ga.ntig.se>
2025-10-09 16:38:35 +0200

    reydtfgu

--- app.rb
+++ app.rb
@@ -42,7 +42,74 @@ get("/fruits") do
   db = SQLite3::Database.new("db/fruits.db")
   db.results_as_hash = true
 
-  @dataFruits = db.execute("SELECT * FROM fruits")
+  query = params[:q]
+  viewAll = params[:viewAll]
+  add = params[:add]
+
+  if add != nil
+    redirect("/fruits/new")
+  end
+
+  if query && !query.empty? && viewAll == nil
+    @dataFruits = db.execute("SELECT * FROM fruits WHERE name LIKE?", "%#{query}%")
+    @searchSucces = true
+    if @dataFruits.empty?
+      @searchSucces = false
+      @dataFruits = db.execute("SELECT * FROM fruits")
+    end
+  else
+    @dataFruits = db.execute("SELECT * FROM fruits")
+    @searchSucces = true
+  end
 
   slim(:"fruits/index")
+end
+
+get("/fruits/new") do
+  slim(:"fruits/new")
+end
+
+post("/fruits/add") do
+  fruitType = params[:type]
+  fruitAmount = params[:amount].to_i
+
+  db = SQLite3::Database.new("db/fruits.db")
+  db.execute("INSERT INTO fruits (name, amount) VALUES (?, ?)", [fruitType, fruitAmount])
+
+  redirect("/fruits")
+end 
+
+# använd id (siffra) för att ta bort frukt
+post("/fruits/:id/delete") do
+  id = params[:id].to_i
+
+  db = SQLite3::Database.new("db/fruits.db")
+  db.execute("DELETE FROM fruits WHERE id =?", id)
+  
+  redirect("/fruits")
+end
+
+get("/fruits/:id/edit") do
+  id = params[:id].to_i
+
+  db = SQLite3::Database.new("db/fruits.db")
+  db.results_as_hash = true
+  @fruitData = db.execute("SELECT * FROM fruits WHERE id = ?", id).first
+
+  slim(:"fruits/edit")
+end
+
+post("/fruits/:id/update") do
+  type = params[:type]
+  id = params[:id].to_i
+  amount = params[:amount].to_i
+
+  db = SQLite3::Database.new("db/fruits.db")
+  db.execute("UPDATE fruits SET name = ?, amount = ? WHERE id = ?", [type, amount, id])
+
+  redirect("/fruits")
+end
+
+post("/fruits/update/back") do
+  redirect("/fruits")
 end
